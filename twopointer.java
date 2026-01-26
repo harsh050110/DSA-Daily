febb8d05b1806615e2798b045d76a590c6302c2b
Harsh Raj <your_github_email@example.com>
2026-01-26 20:28:36 +0530

    Add gitignore for class files

---
+++ twopointer.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class TwoPointer{
+    public static void main(String[]args){
+        TwoPointer tp = new TwoPointer();
+
+        int [] arr ={1,1,2,2,3,4,4,5,6,6};
+        tp .DuplicateRemover(arr);
+
+    } 
+
+    public void DuplicateRemover(int[] arr){
+        int slow =0;
+        for(int fast=1; fast < arr.length; fast++){
+            if(arr[slow] != arr[fast]){
+                slow++;
+                arr[slow] = arr[fast];
+            }
+        }
+        System.out.println(slow+1);
+    }
+        
+}
