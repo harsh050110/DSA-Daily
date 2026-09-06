c9ec1301077e870022c7f3b4c4d3f03896ef6a91
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-06 23:15:32 +0530

    added

---
+++ TP30.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TP30{
+    public static void main(String[] args){
+        String website = "Maintenance";
+
+        if(website != "Online"){
+            System.out.println("The website is not online");
+        } else {
+            System.out.println("The website is online");
+        }
+    }
+}
