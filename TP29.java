8a4f60b944903c871635b8462abe0c81f729dd9f
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-06 23:11:46 +0530

    added

---
+++ TP29.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TP29 {
+    public static void main(String[] args){
+        int number=15;
+
+        if(number!=10){
+            System.out.println("The number is not equal to 10");
+        } else {
+            System.out.println("The number is equal to 10");
+        }
+    }
+}
