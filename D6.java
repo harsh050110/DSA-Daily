a8c0c05e3a9d6859acb5db5624e29c813877ab23
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-08 20:03:41 +0530

    added

---
+++ D6.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class D6{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    
+        int  temperature = 45;
+
+
+        if(temperature > 30){
+            System.out.println("It's a hot day.");
+        }else if(temperature >20){
+            System.out.println("It's a nice day.");
+        }else{
+            System.out.println("It's a cold day.");
+        }
+
+        
+    }
+
+}
