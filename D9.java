1a108225b74c791535502a7ff60b380ad60aee56
Harsh Kumar <harshkumar011045@gmail.com>
2026-09-09 21:24:46 +0530

    added

--- D9.java
+++ D9.java
@@ -1,6 +1,7 @@
 public class D9 {
     public static void main(String[] args){
         pattern1(4);
+        pattern2(5);
 
     }
     static void pattern1(int n){
@@ -12,5 +13,12 @@ public class D9 {
             System.out.println();
         }
     }
-    
+    static void pattern2(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
 }
